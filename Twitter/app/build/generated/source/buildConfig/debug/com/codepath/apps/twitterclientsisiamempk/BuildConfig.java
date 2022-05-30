a521d877f0effd4bb3fbef8c935776c87766eaa6
YerBoySisi <YerBoySisi@users.noreply.github.com>
2022-05-30 10:44:30 -0700

    set up project

---
+++ Twitter/app/build/generated/source/buildConfig/debug/com/codepath/apps/twitterclientsisiamempk/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.codepath.apps.twitterclientsisiamempk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.codepath.apps.restclienttemplate";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String CONSUMER_KEY = "X5KGEH2sLYlWmJGIaJNCWfGm6";
+  // Field from default config.
+  public static final String CONSUMER_SECRET = "AHStA8TOcJki8yQOYIIF9VoZAFUCcejsKuOsaJFJbS41d3Lsrf";
+}
